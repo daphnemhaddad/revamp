35c1668b1eeb80e5b999d0b20dd554f8d40cade0
daphnemhaddad <45691196+daphnemhaddad@users.noreply.github.com>
2019-10-04 11:40:17 -0500

    updated nav
    set the proper php commands for each nav link to hide the current page you are on.
    as well as hiding all links expect home for the sources page

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,28 @@
 	<nav class="navi">
-		<a href="Disco.php">Home</a>
-		<a href="xy.php">X&Y</a>
-		<a href="rushhead.php">A Rush of Blood To The Head</a>
-		<a href="MyloXylo.php">Mylo Xyloto</a>
-	</nav>
+<?php
+	$t = '/';
+	$fp = strstr($_SERVER['PHP_SELF'], $t);
+	$name = stripos($fp, 'source');
+	if (!$name) {
+		echo '<a href="Disco.php">Home</a>';
+		$name = stripos($fp, 'parachute');
+			if (!$name) {
+			echo '<a href="parachutes.php">Parachutes</a>';
+		}
+		$name = stripos($fp, 'xy.php');
+			if (!$name) {
+			echo '<a href="xy.php">X&amp;Y</a>';
+		}
+		$name = stripos($fp, 'rushhead');
+			if (!$name) {
+			echo '<a href="rushhead.php">A Rush of Blood To The Head</a>';
+		}
+		$name = stripos($fp, 'MyloXylo');
+		if (!$name) {
+		echo '<a href="MyloXylo.php">Mylo Xyloto</a>';
+		}
+	} else {
+		echo '<a href="Disco.php">Home</a>';
+	}
+ ?>
+		</nav>
